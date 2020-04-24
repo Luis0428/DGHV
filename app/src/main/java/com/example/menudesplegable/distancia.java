24b48a72e66ed3b06e87c39ec3b6073dd7c5af69
AlejandroHernandez127 <alejandrohernandez127@gmail.com>
2020-04-24 15:38:44 -0500

    Agregada funcion resto de ventanas

--- app/src/main/java/com/example/menudesplegable/distancia.java
+++ app/src/main/java/com/example/menudesplegable/distancia.java
@@ -5,14 +5,90 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class distancia extends Fragment
 {
+    EditText etx1, etx2, ety1, ety2;
+    TextView tvDistancia, tvPendiente;
+    Button btnResolver, btnLimpiar;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_distancia, container, false);
+        View view = inflater.inflate(R.layout.fragment_distancia, container, false);
+
+        etx1 = view.findViewById(R.id.dist_etx1);
+        etx2 = view.findViewById(R.id.dist_etx2);
+        ety1 = view.findViewById(R.id.dist_ety1);
+        ety2 = view.findViewById(R.id.dist_ety2);
+        tvDistancia = view.findViewById(R.id.dist_tvPuntos);
+        tvPendiente = view.findViewById(R.id.dist_tvPendiente);
+        btnResolver = view.findViewById(R.id.dist_btnResolver);
+        btnLimpiar = view.findViewById(R.id.dist_btnLimpiar);
+
+        btnResolver.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        float x1, x2, y1, y2, distancia, pendiente;
+                        validarEntradas();
+
+                        x1 = Float.parseFloat(etx1.getText().toString());
+                        x2 = Float.parseFloat(etx2.getText().toString());
+                        y1 = Float.parseFloat(ety1.getText().toString());
+                        y2 = Float.parseFloat(ety2.getText().toString());
+
+                        distancia = (float) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+                        pendiente = (y2 - y1) / (x2 - x1);
+
+                        tvDistancia.setText(String.valueOf(distancia));
+                        tvPendiente.setText(String.valueOf(pendiente));
+                    }
+                }
+        );
+
+        btnLimpiar.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        etx1.setText("");
+                        etx2.setText("");
+                        ety1.setText("");
+                        ety2.setText("");
+                        tvDistancia.setText("");
+                        tvPendiente.setText("");
+                    }
+                }
+        );
+
+        return view;
+    }
+
+    private void validarEntradas()
+    {
+        String x1 = etx1.getText().toString();
+        String x2 = etx2.getText().toString();
+        String y1 = ety1.getText().toString();
+        String y2 = ety2.getText().toString();
+
+        if (x1.equals(""))
+            etx1.setText("0");
+
+        if (x2.equals(""))
+            etx2.setText("0");
+
+        if (y1.equals(""))
+            ety1.setText("0");
+
+        if (y2.equals(""))
+            ety2.setText("0");
     }
 }
--- app/src/main/java/com/example/menudesplegable/pitagoras.java
+++ app/src/main/java/com/example/menudesplegable/pitagoras.java
@@ -5,14 +5,113 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class pitagoras extends Fragment
 {
-@Override
+    EditText etA, etB, etC;
+    RadioButton rbA, rbB, rbC;
+    TextView tvSalida, tvRes;
+    Button btnResolver, btnLimpiar;
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pitagoras, container, false);
+        View vista = inflater.inflate(R.layout.fragment_pitagoras, container, false);
+
+        etA = vista.findViewById(R.id.pit_etA);
+        etB = vista.findViewById(R.id.pit_etB);
+        etC = vista.findViewById(R.id.pit_etC);
+        rbA = vista.findViewById(R.id.pit_rbA);
+        rbB = vista.findViewById(R.id.pit_rbB);
+        rbC = vista.findViewById(R.id.pit_rbC);
+        tvSalida = vista.findViewById(R.id.pit_tvSalida);
+        tvRes = vista.findViewById(R.id.pit_tvRes);
+        btnResolver = vista.findViewById(R.id.pit_btnResolver);
+        btnLimpiar = vista.findViewById(R.id.pit_btnLimpiar);
+
+        btnResolver.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        validarEntradas();
+                        float valor1, valor2, res, tmp;
+
+                        if(rbA.isChecked())
+                        {
+                            valor1 = Float.parseFloat(etB.getText().toString());
+                            valor2 = Float.parseFloat(etC.getText().toString());
+
+                            tmp = (float) (Math.pow(valor2, 2) - Math.pow(valor1, 2));
+                            res = (float) Math.sqrt(tmp);
+
+                            tvSalida.setText("a:");
+                            tvRes.setText(String.valueOf(res));
+                        }
+                        else if(rbB.isChecked())
+                        {
+                            valor1 = Float.parseFloat(etA.getText().toString());
+                            valor2 = Float.parseFloat(etC.getText().toString());
+
+                            tmp = (float) (Math.pow(valor2, 2) - Math.pow(valor1, 2));
+                            res = (float) Math.sqrt(tmp);
+
+                            tvSalida.setText("b:");
+                            tvRes.setText(String.valueOf(res));
+                        }
+                        else if(rbC.isChecked())
+                        {
+                            valor1 = Float.parseFloat(etA.getText().toString());
+                            valor2 = Float.parseFloat(etB.getText().toString());
+
+                            tmp = (float) (Math.pow(valor2, 2) + Math.pow(valor1, 2));
+                            res = (float) Math.sqrt(tmp);
+
+                            tvSalida.setText("c:");
+                            tvRes.setText(String.valueOf(res));
+                        }
+                    }
+                }
+        );
+
+        btnLimpiar.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        etA.setText("");
+                        etB.setText("");
+                        etC.setText("");
+
+                        tvRes.setText("");
+                        tvSalida.setText("a/b/c:");
+                    }
+                }
+        );
+
+        return vista;
+    }
+
+    private void validarEntradas()
+    {
+        String a = etA.getText().toString();
+        String b = etB.getText().toString();
+        String c = etC.getText().toString();
+
+        if(a.equals(""))
+            etA.setText("0");
+
+        if(b.equals(""))
+            etB.setText("0");
+
+        if(c.equals(""))
+            etC.setText("0");
     }
 }
